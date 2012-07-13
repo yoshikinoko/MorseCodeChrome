0f9b442d89228efc38f834ded6b6b890b1a7889f
Yoshiki TAKEOKA <yoshikinoko@gmail.com>
2012-07-13 14:10:30 +0900

    Initial Development for Chrome Extenstion

--- README.markdown
+++ README.markdown
@@ -1,37 +1,20 @@
-# Morse.js
-## A jQuery Plugin that annotates text with Morse Code
+# MorseCodeChrome
+## A Chrome extension that transcribes text with Morse Code
 
 Samuel F. B. Morse created a code for the electric telegraph in the early 1840s that allowed alphanumeric characters to be encoded into a sequence of short and long tones. It is still widely used among radio operators as a means of identifying themselves, as well as communicating messages over a channel too narrow or noisy for speech transmission.[<sup>1</sup>](http://en.wikipedia.org/wiki/Morse_code)
 
-This plugin will transcribe the morse code for text in the selected elements. In browsers that support the [`<ruby>` HTML element](http://www.w3.org/TR/1998/WD-ruby-19981221/), the transcription will appear above the text. By default, clicking on these elements will play the corresponding audio for the transcription.
+This extension will transcribe the morse code for text in the page. 
 
-## Demo
+## Install from chrome web store
 
-Bleeps and bloops speak louder than words:
-
-Try it out at: [http://mattt.github.com/Morse.js/](http://mattt.github.com/Morse.js/ "Try out Morse.js!")  
-*Note:* Requires browser with support for HTML5 `<audio>`, such as [Safari 4](http://www.apple.com/safari/download/), [Firefox 3.5](http://www.mozilla.com/firefox/), or [Chrome](http://www.google.com/chrome/)
-
-## Usage
-
-    $("p").morseCode({bpm:12});
-
-- `bpm` rate at which the message is played (default: 12, or 1 unit = 100ms)
-
-In addition, there are two namespaced events that you can `trigger` and `bind` to:
-
-- `morse.emit` will generate and play the tones for morse code elements
-- `morse.mute` will stop morse code tone sounds from `<audio>` elements
-
-## Requirements
-
-- jQuery 1.4+
+ [chrome web store](https://chrome.google.com/webstore/detail/bbeopnnpdidnlidlkjgkbmlcgielnoko "Try out Morse Code Extension")  
 
 ## License
 
-Morse.js is licensed under the MIT License:
+MorseCodeChrome is licensed under the MIT License:
 
-  Copyright (c) 2010 Mattt Thompson (http://mattt.me/)
+
+  Copyright (c) 2012 Yoshiki Takeoka (http://www.yoshikit.net/)
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
@@ -53,7 +36,4 @@ Morse.js is licensed under the MIT License:
 
 ## Credit
 
-Javascript client-side WAV generation based on code by sk89q
-See [http://sk89q.therisenrealm.com/](http://sk89q.therisenrealm.com/) for more info.
-
-Thanks to [Justin Slepak](https://github.com/jrslepak) for adding support for punctuation characters.
+This program was forked from Morse.js by Mattt Thompson (https://github.com/mattt/morse.js/) 
--- morse.js
+++ morse.js
@@ -1,16 +1,4 @@
-/*
- * Morse.js
- * A jQuery Plugin that annotates text with Morse Code
- * http://github.com/mattt/Morse.js
- *
- * Copyright (c) 2010 Mattt Thompson
- * Licensed under the MIT licenses.
- */
-
-(function($){
-  Morse = {
-    wpm: 12,
-    code: {
+var code = {
           "a": "._",    "b": "_...",  "c": "_._.",  "d": "_..",
           "e": ".",     "f": ".._.",  "g": "__.",   "h": "....",
           "i": "..",    "j": ".___",  "k": "_._",   "l": "._..",
@@ -33,151 +21,70 @@
           ":": "___...", ";": "_._._.", "=": "_..._",   "+": "._._.",
           "-": "_...._", "_": "..__._", "\"": "._.._.", "$": "..._.._",
           "!": "_._.__", "@": ".__._."
-    },
-    annotate: function(el) {
-      var $el = $(el);
-      var tokens = $el.text().split(/\s+/);
+};
+var skiptag = [ "HTML", "SCRIPT", "HEAD", "META", "BODY", "NOSCRIPT", "IMG",
+		"STYLE", "LINK" ];
+function translate_morse(el) {
 
-      $el.text('');
+	var tokens = el.split(/\s+/);
 
-      for(i in tokens) {
-        var token = tokens[i];
-        var letters = token.split('');
-        var symbols = [];
+	var result = "";
+	for (i in tokens) {
 
-        for(j in letters) {
-          var letter = letters[j];
-          var symbol = Morse.code[letter.toLowerCase()];
-          if(symbol) {
-            symbols.push(symbol);
-          }
-        }
+		var token = tokens[i];
+		var letters = token.split('');
+		var symbols = [];
 
-        $el.append('<ruby class="morse-code"><rb>' + token + '</rb><rt>' + symbols.join(' ') + '&nbsp;' + '</rt></ruby>');
-      }
+		for (j in letters) {
+			var letter = letters[j];
+			var symbol = code[letter.toLowerCase()];
+			if (symbol) {
+				symbols.push(symbol);
+			}else{
+				symbols.push(letter);
+			}
+		}
+		result = result.concat(" " + symbols.join(''));
+	}
 
-      $el.each(function() {
-        if($("#morse-code-output")[0] === undefined) {
-          var audio = $('<audio id="morse-code-output"></audio>').bind("morse.mute", function(){this.pause();})
-          $(this).after(audio);
-        }
-        $(this).bind('morse.emit', Morse.emit).bind('click', function(){ $(this).trigger("morse.emit")});
-      });
-    },
-    emit: function() {
-      $("#morse-code-output").trigger("morse.mute").attr('src', "");
-      var symbols = [];
-      $(this).find("rt").each(function() {
-        symbols.push($(this).text());
-      })
+	return result;
+};
+function skiptagcheck(v) {
+	for ( var i = 0; i < skiptag.length; i++) {
+		if (skiptag[i] == v) {
+			return true;
+		}
+	}
+	return false;
+}
 
-      // Javascript WAV generation based on code by sk89q (http://sk89q.therisenrealm.com/)
-      var generate = function(symbols, options) {
-        var defaults = {
-          channels: 1,
-          sampleRate: 1012,
-          bitDepth: 16,
-          unit: 0.100,
-          frequency: 440.0,
-          volume: 32767
-        };
+function gettree(e) {
+	var max = e.length;
+	for ( var j = 0; j < max; j++) {
+		var vl = e[j];
+		if (vl.nodeType == Node.TEXT_NODE) {
+			var ttt = "" + vl.nodeValue;
+			vl.nodeValue = translate_morse(ttt);
+		}
+	}
+};
 
-        var options = $.extend(defaults, options);
+function replaceMorse() {
+	var elm = document.getElementsByTagName("*");
+	var ell = elm.length;
+	for ( var i = 0; i < ell; i++) {
+		var val = elm[i];
+		var str = val.nodeName;
 
-        var channels      = options.channels;
-        var sampleRate    = options.sampleRate;
-        var bitsPerSample = options.bitDepth;
-        var unit          = options.unit;
-        var frequency     = options.frequency;
-        var volume        = options.volume;
+		if (!skiptagcheck(str)) {
+			var et = val.childNodes;
+			gettree(et);
+		}
+	}
 
-        var data = [];
-        var samples = 0;
-
-        var tone = function(length) {
-          for (var i = 0; i < sampleRate * unit * length; i++) {
-            for (var c = 0; c < channels; c++) {
-                var v = volume * Math.sin((2 * Math.PI) * (i / sampleRate) * frequency);
-                data.push(pack("v", v)); samples++;
-            }
-          }
-        }
-
-        var silence = function(length) {
-          for (var i = 0; i < sampleRate * unit * length; i++) {
-            for (var c = 0; c < channels; c++) {
-                data.push(pack("v", 0)); samples++;
-            }
-          }
-        }
-
-        for (var i in symbols) {
-          var length;
-          var symbol = symbols[i];
-          if(symbol == '|') {
-            silence(7);
-          } else {
-            if(symbol == '.') {
-              tone(1);
-              silence(1);
-            } else if(symbol == '_') {
-              tone(3);
-              silence(1);
-            } else {
-              silence(3);
-            }
-          }
-        }
-
-        data = data.join('');
-
-        // Format sub-chunk
-        var chunk1 = [
-            "fmt ", // Sub-chunk identifier
-            pack("V", 16), // Chunk length
-            pack("v", 1), // Audio format (1 is linear quantization)
-            pack("v", channels),
-            pack("V", sampleRate),
-            pack("V", sampleRate * channels * bitsPerSample / 8), // Byte rate
-            pack("v", channels * bitsPerSample / 8),
-            pack("v", bitsPerSample)
-        ].join('');
-
-        // Data sub-chunk (contains the sound)
-        var chunk2 = [
-            "data", // Sub-chunk identifier
-            pack("V", samples * channels * bitsPerSample / 8), // Chunk length
-            data
-        ].join('');
-
-        // Header
-        var header = [
-            "RIFF",
-            pack("V", 4 + (8 + chunk1.length) + (8 + chunk2.length)), // Length
-            "WAVE"
-        ].join('');
-
-        return "data:audio/wav;base64," + escape(btoa([header, chunk1, chunk2].join('')));
-      };
-
-      // Base 64 encoding function, for browsers that do not support btoa()
-      // by Tyler Akins (http://rumkin.com), available in the public domain
-      var btoa=function(b){var c="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",h="",a,d,e,j,i,f,g=0;do{a=b.charCodeAt(g++);d=b.charCodeAt(g++);e=b.charCodeAt(g++);j=a>>2;a=(a&3)<<4|d>>4;i=(d&15)<<2|e>>6;f=e&63;if(isNaN(d))i=f=64;else if(isNaN(e))f=64;h=h+c.charAt(j)+c.charAt(a)+c.charAt(i)+c.charAt(f)}while(g<b.length);return h};
-
-
-      // pack() emulation (from the PHP version), for binary crunching
-      var pack = function(e){for(var b="",c=1,d=0;d<e.length;d++){var f=e.charAt(d),a=arguments[c];c++;switch(f){case "a":b+=a[0]+"\u0000";break;case "A":b+=a[0]+" ";break;case "C":case "c":b+=String.fromCharCode(a);break;case "n":b+=String.fromCharCode(a>>8&255,a&255);break;case "v":b+=String.fromCharCode(a&255,a>>8&255);break;case "N":b+=String.fromCharCode(a>>24&255,a>>16&255,a>>8&255,a&255);break;case "V":b+=String.fromCharCode(a&255,a>>8&255,a>>16&255,a>>24&255);break;case "x":c--;b+="\u0000";break;default:throw new Error("Unknown pack format character '"+
-      f+"'");}}return b};
-
-      $("#morse-code-output").attr('src', generate(symbols.join('|'), {unit: 1.200 / Morse.wpm}))[0].play();
-    },
-  };
-
-  $.fn.extend({
-    morseCode: function() {
-      return this.each(function(){
-        (new Morse.annotate(this));
-      });
-    }
-  });
-})(jQuery);
+};
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript(null, { file: 'morse.js' }, function(){
+	  chrome.tabs.executeScript(null, {code:'replaceMorse()'});
+	  });
+});
